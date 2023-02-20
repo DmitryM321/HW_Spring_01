443342e047e9f6162d73f9a6a0f9f48bc7b0bfd6
Dmitry <a89297175356@gmail.com>
2023-02-20 17:14:09 +0500

    ДЗ 01_03

--- src/main/java/org/example/intoduction/Main.java
+++ src/main/java/org/example/intoduction/Main.java
@@ -4,9 +4,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Часть 2.  внедрение зависимостей
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-        Car car = context.getBean(Car.class);
-        car.go();
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        Person person1 = context.getBean("customPerson", Person.class);
+        person1.drive();
+
+        // Часть1
+//        ClassPathXmlApplicationContext context =
+//                new ClassPathXmlApplicationContext("applicationContext.xml");
+//        Car car = context.getBean(Car.class);
+//        car.go();
+//        context.close();
     }
 }
